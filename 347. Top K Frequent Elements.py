a7ec22df0442a1dc4b53385ad7a96d1ffc2fb7da
SuvorovJenya <e.s.suvorov@mail.ru>
2025-05-05 13:09:24 +0300

    change Top K Frequent Elements"

--- 347. Top K Frequent Elements.py
+++ 347. Top K Frequent Elements.py
@@ -1,6 +1,8 @@
 from collections import Counter
+
+
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+    def topKFrequent(self, nums, k):
         result = []
         counter_map = Counter([str(i) for i in nums])
         for i in counter_map.most_common(k):
